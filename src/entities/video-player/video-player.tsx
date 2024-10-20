28b432579eaaefbfef90d150628528e89b77ef2a
Emilia <81705094+isemilia@users.noreply.github.com>
2024-10-20 12:12:51 +0500

    Pause and play the video

--- src/entities/video-player/video-player.tsx
+++ src/entities/video-player/video-player.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
 import { TVideoPlayerProps } from "./model/types.ts";
 import {
   StyledControls,
@@ -10,19 +10,35 @@ import {
 import PlayButton from "./ui/play-button";
 
 const VideoPlayer: FC<TVideoPlayerProps> = ({ source }) => {
+  const videoRef = useRef<null | HTMLVideoElement>(null);
+
+  const [isPaused, setPaused] = useState(true);
+
   return (
     <StyledWrap class={"video-root"}>
       <StyledVideoOverlay>
         <StyledVideoFooter>
           <StyledTimeline></StyledTimeline>
           <StyledControls>
-            <PlayButton isPaused={true} />
+            <PlayButton
+              isPaused={isPaused}
+              onClick={() => {
+                if (!videoRef.current) return;
+
+                if (isPaused) {
+                  videoRef.current?.play();
+                } else {
+                  videoRef.current?.pause();
+                }
+                setPaused((state) => !state);
+              }}
+            />
             {/*<PlayButton isPaused={true} />*/}
           </StyledControls>
         </StyledVideoFooter>
       </StyledVideoOverlay>
 
-      <video src={source}></video>
+      <video ref={videoRef} src={source}></video>
     </StyledWrap>
   );
 };
